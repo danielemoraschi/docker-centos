63b04e79f892f5374fd8d5b4b4c4163258324fce
danielemoraschi <daniele.moraschi@gmail.com>
2016-02-27 21:39:44 -0500

    nginx changed default vhost conf file

--- nginx/scripts/nginx-start.sh
+++ nginx/scripts/nginx-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/conf.d/app.conf
+sed -i "s/%fpm-ip%/$FPM_PORT_9000_TCP_ADDR/" /etc/nginx/conf.d/default.conf
 
 exec /usr/sbin/nginx
